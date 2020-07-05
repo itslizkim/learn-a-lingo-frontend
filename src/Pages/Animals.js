eaf4ad3dfadff116cacda005c77c4709d12cd613
itslizkim <elizabeth.kim93@gmail.com>
2020-07-04 21:54:55 -0400

    deleted flashcard file in components

--- src/Pages/Animals.js
+++ src/Pages/Animals.js
@@ -35,7 +35,7 @@ const Animals = () => {
             <img alt="oh no!" src={img_url}/>
             <div className="word"><h1>{word}</h1></div>
             <br></br>
-            <audio id="audio" src={audio} controls>play</audio>
+            {/* <audio id="audio" src={audio} controls>play</audio> */}
         </div>
     )
   }
